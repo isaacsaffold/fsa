bb683462422f38451f402ca928135761757b0c1f
Isaac <ijsaffold@mail.lipscomb.edu>
2019-03-07 19:43:29 -0600

    alphabet no longer sorted, as its symbols may not be totally ordered

--- dfa.py
+++ dfa.py
@@ -115,7 +115,7 @@ class DFA:
         # Iterables may be lazily evaluated, hence unusual idioms for
         # initializing locals below.
         state_indices = dict(zip(states, count()))
-        alphabet = tuple(sorted(alphabet))
+        alphabet = tuple(alphabet)
         sym_indices = dict(zip(alphabet, count()))
         trans_matrix = [[0] * len(alphabet) for i in range(len(state_indices))]
         for orig, sym, dest in transitions:
